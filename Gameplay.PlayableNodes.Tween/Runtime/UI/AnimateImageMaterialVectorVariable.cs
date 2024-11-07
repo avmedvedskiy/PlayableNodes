d0df796c3d3f250eabce203320d8f7e00444e44f
avmedvedskiy <a.v.medvedskiy@gmail.com>
2024-11-07 17:35:57 +0200

    add change material

---
+++ Gameplay.PlayableNodes.Tween/Runtime/UI/AnimateImageMaterialVectorVariable.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using DG.Tweening;
+using PlayableNodes.Values;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace PlayableNodes
+{
+    [Serializable]
+    public class AnimateImageMaterialVectorVariable : TweenAnimation<Image>
+    {
+        [SerializeField] private Material _material;
+        [SerializeField] private Vector3 _from;
+        [SerializeField] private Vector3 _to;
+        [SerializeField] private string _fieldName;
+
+        private Vector3 _current;
+        private Material _lastMaterial;
+
+        protected override Tween GenerateTween()
+        {
+            _current = _from;
+            return DOTween
+                .To(() => _current, SetVector, _to, Duration)
+                .OnStart(OnStart)
+                .OnComplete(OnComplete)
+                .ChangeStartValue(_current);
+        }
+
+        private void OnComplete()
+        {
+            if (Target)
+                Target.material = _lastMaterial;
+        }
+
+        private void OnStart()
+        {
+            _lastMaterial = Target.material;
+            Target.material = _material;
+        }
+
+        private void SetVector(Vector3 value)
+        {
+            _current = value;
+            Target.material.SetVector(_fieldName, value);
+        }
+    }
+}
