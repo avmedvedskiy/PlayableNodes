bef9055a43d8925e3c788fe6a1b528975e8e0ef5
avmedvedskiy <a.v.medvedskiy@gmail.com>
2024-11-14 00:37:48 +0200

    add rebuild layout

---
+++ Gameplay.PlayableNodes.Tween/Runtime/Transform/RectTransform/RebuildLayoutEveryFrame.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace PlayableNodes
+{
+    [Serializable]
+    public class RebuildLayoutEveryFrame : TargetAnimation<RectTransform>
+    {
+        private float _time;
+
+        protected override async UniTask Play(CancellationToken cancellationToken)
+        {
+            while (!cancellationToken.IsCancellationRequested && _time < Duration)
+            {
+                LayoutRebuilder.MarkLayoutForRebuild(Target);
+                await UniTask.Yield(PlayerLoopTiming.Update);
+                _time += Time.deltaTime;
+            }
+        }
+    }
+}
