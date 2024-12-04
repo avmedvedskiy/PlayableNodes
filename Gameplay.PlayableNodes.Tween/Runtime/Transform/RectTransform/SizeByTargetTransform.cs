cff6a2310019d57ad08c0e6610d11dcde90ca82e
avmedvedskiy <a.v.medvedskiy@gmail.com>
2024-12-05 01:08:06 +0200

    add sizebytarget axis constraint

--- Gameplay.PlayableNodes.Tween/Runtime/Transform/RectTransform/SizeByTargetTransform.cs
+++ Gameplay.PlayableNodes.Tween/Runtime/Transform/RectTransform/SizeByTargetTransform.cs
@@ -10,13 +10,16 @@ namespace PlayableNodes
     {
         [SerializeField] private ToFromValue<Vector2> _from ;
         [SerializeField] private Transform _to;
+        [SerializeField] private Vector2 _additionalSize;
+        [SerializeField] private AxisConstraint _axisConstraint = AxisConstraint.None;
 
         private RectTransform To =>(RectTransform)_to;
 
         protected override Tween GenerateTween() =>
             Target
-                .DOSizeDelta(To.sizeDelta, Duration)
-                .ChangeValuesVector(To.sizeDelta, _from);
+                .DOSizeDelta(To.sizeDelta + _additionalSize, Duration)
+                .SetOptions(_axisConstraint)
+                .ChangeValuesVector(To.sizeDelta + _additionalSize, _from);
         
 
         public void ChangeEndValue(Transform value) => _to = value;
