eb86a16a1f14d75551abc482ffc2b46ffdac9fc7
avmedvedskiy <a.v.medvedskiy@gmail.com>
2025-03-27 16:48:52 +0200

    add random force node

--- Gameplay.PlayableNodes.Tween/Runtime/Rigidbody/AddForceRigidbody.cs
+++ Gameplay.PlayableNodes.Tween/Runtime/Rigidbody/AddForceRigidbody.cs
@@ -2,14 +2,35 @@
 using System.Threading;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace PlayableNodes
 {
+    [Serializable]
+    public class AddRandomForceRigidbody : TargetAnimation<Rigidbody>
+    {
+        [SerializeField] private Vector3 _forceMin;
+        [SerializeField] private Vector3 _forceMax;
+        [SerializeField] private ForceMode _mode = ForceMode.Impulse;
+
+        protected override UniTask Play(CancellationToken cancellationToken)
+        {
+            Target.isKinematic = false;
+            Target.AddForce(new Vector3(
+                Random.Range(_forceMin.x, _forceMax.x),
+                Random.Range(_forceMin.y, _forceMax.y),
+                Random.Range(_forceMin.z, _forceMax.z)
+            ), _mode);
+            return UniTask.WaitForSeconds(Duration, cancellationToken: cancellationToken);
+        }
+    }
+
     [Serializable]
     public class AddForceRigidbody : TargetAnimation<Rigidbody>
     {
         [SerializeField] private Vector3 _force;
         [SerializeField] private ForceMode _mode = ForceMode.Impulse;
+
         protected override UniTask Play(CancellationToken cancellationToken)
         {
             Target.isKinematic = false;
