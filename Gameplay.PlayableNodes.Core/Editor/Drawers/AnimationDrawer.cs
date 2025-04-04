86d742efc76f868449b88ca28bdbb817f7fbbe80
avmedvedskiy <a.v.medvedskiy@gmail.com>
2025-04-04 12:35:25 +0300

    fix namespace errors

--- Gameplay.PlayableNodes.Core/Editor/Drawers/AnimationDrawer.cs
+++ Gameplay.PlayableNodes.Core/Editor/Drawers/AnimationDrawer.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
 using System.Reflection;
-using DG.DemiEditor;
-using ManagedReference;
 using ManagedReference.Editor;
 using UnityEditor;
 using UnityEditorInternal;
