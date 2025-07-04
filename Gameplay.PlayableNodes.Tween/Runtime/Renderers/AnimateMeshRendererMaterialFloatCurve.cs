98842b0e7276059b070cb001452e36fa6f52dc68
Andrew Medvedskiy <a.v.medvedskiy@gmail.com>
2025-07-04 14:52:59 +0300

    Allow using target material when none specified

---
+++ Gameplay.PlayableNodes.Tween/Runtime/Renderers/AnimateMeshRendererMaterialFloatCurve.cs
@@ -0,0 +1,52 @@
+using System;
+using System.ComponentModel;
+using DG.Tweening;
+using PlayableNodes.Animations;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace PlayableNodes
+{
+    [Serializable]
+    [Description("Tweens a float material property on a MeshRenderer using an AnimationCurve and optionally restores the original material on completion")]
+    public class AnimateMeshRendererMaterialFloatCurve : TweenAnimation<MeshRenderer>
+    {
+        [SerializeField] private Material _material;
+        [SerializeField] private AnimationCurve _curve = AnimationCurve.Linear(0f, 0f, 1f, 1f);
+        [SerializeField] private string _fieldName;
+        [SerializeField] private bool _resetMaterialOnComplete = true;
+
+        private Material _lastMaterial;
+
+        protected override Tween GenerateTween()
+        {
+            float t = 0f;
+            return DOTween
+                .To(() => t, x => Set(_curve.Evaluate(x)), 1f, Duration)
+                .OnStart(OnStart)
+                .OnComplete(OnComplete);
+        }
+
+        private void OnStart()
+        {
+            _lastMaterial = Target.sharedMaterial;
+            var baseMaterial = _material == null ? _lastMaterial : _material;
+            Target.material = Object.Instantiate(baseMaterial);
+            Set(_curve.Evaluate(0f));
+        }
+
+        private void OnComplete()
+        {
+            if (Target && _resetMaterialOnComplete)
+            {
+                Target.material.DestroyOrPreview();
+                Target.material = _lastMaterial;
+            }
+        }
+
+        private void Set(float value)
+        {
+            Target.material.SetFloat(_fieldName, value);
+        }
+    }
+}
---
+++ Gameplay.PlayableNodes.Tween/Runtime/Renderers/AnimateMeshRendererMaterialFloatVariable.cs
@@ -0,0 +1,53 @@
+using System;
+using System.ComponentModel;
+using DG.Tweening;
+using PlayableNodes.Animations;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace PlayableNodes
+{
+    [Serializable]
+    [Description("Tweens a float property on a MeshRenderer material and optionally restores the original material on completion")]
+    public class AnimateMeshRendererMaterialFloatVariable : TweenAnimation<MeshRenderer>
+    {
+        [SerializeField] private Material _material;
+        [SerializeField] private float _from;
+        [SerializeField] private float _to;
+        [SerializeField] private string _fieldName;
+        [SerializeField] private bool _resetMaterialOnComplete = true;
+
+        private Material _lastMaterial;
+
+        protected override Tween GenerateTween()
+        {
+            return DOTween
+                .To(() => _from, Set, _to, Duration)
+                .OnStart(OnStart)
+                .OnComplete(OnComplete)
+                .ChangeStartValue(_from);
+        }
+
+        private void OnStart()
+        {
+            _lastMaterial = Target.sharedMaterial;
+            var baseMaterial = _material == null ? _lastMaterial : _material;
+            Target.material = Object.Instantiate(baseMaterial);
+            Set(_from);
+        }
+
+        private void OnComplete()
+        {
+            if (Target && _resetMaterialOnComplete)
+            {
+                Target.material.DestroyOrPreview();
+                Target.material = _lastMaterial;
+            }
+        }
+
+        private void Set(float value)
+        {
+            Target.material.SetFloat(_fieldName, value);
+        }
+    }
+}
